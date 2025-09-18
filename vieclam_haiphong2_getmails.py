225d744993f632b13a38cd2e754a6d3482efeb69
Minh Anh <vuongminhanhsnail@gmail.com>
2025-09-18 11:12:57 +0700

    20250918_vieclamhaiphong

--- vieclam_haiphong2_getmails.py
+++ vieclam_haiphong2_getmails.py
@@ -10,7 +10,7 @@ import pandas as pd
 #     "https://www.tuyendunghaiphong.vn/viec-lam/910250/tuyen-ky-su-qa-qc-lam-viec-tai-phan-xuong-thanh-pham",
 #     "https://www.tuyendunghaiphong.vn/viec-lam/910374/tuyen-dung-nhan-vien-hanh-chinh-nhan-su-uu-tien-biet-tieng-nhat-tuong-duong-n3"
 # ]
-urls = pd.read_excel("2tuyendung_haiphong3to100.xlsx")["url"].tolist()
+urls = pd.read_excel("input.xlsx")["url"].tolist()
 
 # Khởi tạo driver
 driver = webdriver.Chrome()
@@ -37,6 +37,6 @@ driver.quit()
 
 # Xuất ra file Excel
 df = pd.DataFrame(results)
-df.to_excel("emails_tuyendung_2tuyendung_haiphong3to100.xlsx", index=False)
+df.to_excel("emails_tuyendung_22tuyendung_haiphong501to620.xlsx", index=False)
 
 print("✅ Đã lưu kết quả vào emails_tuyendung.xlsx")
